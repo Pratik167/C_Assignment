4347783c2b2fb172c88c9812862488450f1d54da
Pratik Man Shrestha <pratikshrestha811@gmail.com>
2024-03-09 18:46:31 +0545

    new

---
+++ Assignment/Structure/3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct time_struct 
+{
+    int hour;
+    int min;
+    int sec;
+};
+
+int main() 
+{
+    struct time_struct time;
+    printf("Enter time in second ")
+
+    return 0;
+}
+
